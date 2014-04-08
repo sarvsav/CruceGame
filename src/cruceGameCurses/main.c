74f2247c5d42a537b6093b123a95f63978a66793
sarvsav <sarvsav@gmail.com>
2014-04-08 08:13:49 +0530

    added options and gameplay to gamelogic

--- src/cruceGameCurses/main.c
+++ src/cruceGameCurses/main.c
@@ -1,8 +1,4 @@
-#include "cli.h"
-#include <curses.h>
-#include <locale.h>
-#include <string.h>
-#include <stddef.h>
+#include "gameLogic.h"
 
 #if defined(WIN32) && defined(NDEBUG)
 #include <Windows.h>
@@ -12,112 +8,11 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 int main(int argc, char *argv[])
 #endif
 { 
-    cruceGameOptions(argc,argv);
-    setlocale(LC_ALL, "");
-    initscr();
-    cbreak();
-
-    if (has_colors() == FALSE) {
-        endwin();
-        printf("Your terminal does not support colors!");
-        return 0;
-    }
-   
-    start_color();
-    init_pair(1, COLOR_RED, COLOR_BLACK);
-    refresh();
-
-    welcomeMessage();
-    int limitScore  = getScoreLimit();
-    int noOfPlayers = getNoOfPlayers();
-
-    struct Game *game = game_createGame(limitScore);
-    for (int i = 0; i < noOfPlayers; i++) {
-        int err;
-        while ((err = game_addPlayer(newPlayer(i + 1), game)) == DUPLICATE_NAME)
-             printw("The player's name have to be unique\n");
-        err = game_addPlayer(newPlayer(i + 1), game);
-        if (err != 0)
-            printw("ERROR: game_addPlayer() %d\n", err);
-    }
-    formTeams(game);
-
-    while (!game_winningTeam(game)) {
-        game->round = round_createRound();
-
-        struct Deck *deck = deck_createDeck();
-        deck_deckShuffle(deck);
-
-        memcpy(game->round->players, game->players, 
-               sizeof(struct Player*) * game->numberPlayers);
-        round_distributeDeck(deck, game->round);
-        clear();
-        refresh();
-
-        for (int i = 0; i < game->numberPlayers; i++) {
-            getBid(game, i);
-            clear();
-            refresh();
-        }
-
-        struct Player *bidWinner = round_getBidWinner(game->round);
-        int first = round_findPlayerIndexRound(bidWinner, game->round);
-        for (int i = 0; team_hasCards(game->players[0]); i++) {
-            round_arrangePlayersHand(game->round, first);
-
-            for (int j = 0; j < game->numberPlayers; j++) {
-                printScore(game, game->round);
-                displayCardsAndPickCard(game, j);
-                clear();
-                refresh();
-            }
-
-            struct Player *handWinner = round_handWinner(game->round->hands[i],
-                                                         game->round->trump, 
-                                                         game->round);
-            first = round_findPlayerIndexRound(handWinner, game->round);
-
-            if (deck_cardsNumber(deck) > 0)
-                round_distributeCard(deck, game->round);
-
-        }
-        
-        int oldScore[MAX_GAME_PLAYERS];
-        for(int i = 0; i < MAX_GAME_PLAYERS; i++) {
-            if(game->round->players[i] != NULL) {
-                oldScore[i] = game->round->players[i]->score;
-            }
-        }
-     
-        for (int i = 0; i < MAX_GAME_PLAYERS; i++) {
-            if (game->round->players[i] != NULL && 
-                game->round->players[i] != bidWinner) {
-                game->round->players[i]->score += 
-                    game->round->pointsNumber[i] / 33;
-            } else if (game->round->players[i] == bidWinner) {
-                if (game->round->bids[i] <= game->round->pointsNumber[i] / 33)
-                    bidWinner->score += game->round->pointsNumber[i] / 33;
-                else
-                    bidWinner->score -= game->round->bids[i];
-            }
-        }
-
-        printRoundTerminationMessage(game->round, oldScore);
-        getch();
-
-        deck_deleteDeck(&deck);
-        round_deleteRound(&game->round);
-    }
-
-    for (int i = 0; i < MAX_GAME_PLAYERS; i++)
-        if (game->players[i])
-            team_deletePlayer(&game->players[i]);
-    for (int i = 0; i < MAX_GAME_TEAMS; i++)
-        if (game->teams[i])
-            team_deleteTeam(&game->teams[i]);
-    game_deleteGame(&game);
-
-    getch();
-    endwin();
-    return 0;
+	if (argc >= 2) {
+    	cruceGameOptions(argc,argv);
+	}
+	else {
+		cruceGameLogic();
+	}
+    return EXIT_SUCCESS;
 }
